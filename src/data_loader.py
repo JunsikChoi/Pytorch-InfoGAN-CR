e9d7b50d364816fbcd312705d30a50c4ff4ded7d
Junsikchoi <junsikchoi@korea.ac.kr>
2019-11-25 15:56:48 +0900

    Add dataloader for dSprites dataset

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,26 +1,82 @@
 import torch
 import os
+import sys
+import numpy as np
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
 from torchvision import datasets
+from urllib import request
 
 
-def get_loader(batch_size, root):
-    # Configure data loader
-    # os.makedirs("../data", exist_ok=True)
-    # transforms.Normalize([0.5], [0.5])
-    data_dir = os.path.join(root, 'data/mnist')
+def get_loader(batch_size, root, dataset):
+
+    data_dir = os.path.join(root, 'data')
     os.makedirs(data_dir, exist_ok=True)
-    dataloader = torch.utils.data.DataLoader(
-        datasets.MNIST(
-            data_dir,
-            train=True,
-            download=True,
-            transform=transforms.Compose(
-                [transforms.ToTensor()]
+
+    if dataset == 'mnist':
+        save_dir = f'{data_dir}/mnist'
+        os.makedirs(save_dir, exist_ok=True)
+        dataloader = torch.utils.data.DataLoader(
+            datasets.MNIST(
+                f'{data_dir}/mnist',
+                train=True,
+                download=True,
+                transform=transforms.Compose(
+                    [transforms.ToTensor()]
+                ),
             ),
-        ),
-        batch_size=batch_size,
-        shuffle=True,
-    )
+            batch_size=batch_size,
+            shuffle=True,
+        )
+    elif dataset == 'dsprites':
+        save_dir = f'{data_dir}/dsprites'
+        os.makedirs(save_dir, exist_ok=True)
+        dset = DSpriteDataset(save_dir)
+        dataloader = 'null'
+
     return dataloader
+
+
+class DSpriteDataset(Dataset):
+    """
+    A PyTorch wrapper for the dSprites dataset by
+    Matthey et al. 2017. The dataset provides a 2D scene
+    with a sprite under different transformations:
+    * color
+    * shape
+    * scale
+    * orientation
+    * x-position
+    * y-position
+    """
+
+    def __init__(self, save_dir, transform=None):
+        self.transform = transform
+        self.file_loc = f'{save_dir}/dsprites.npz'
+        url = "https://github.com/deepmind/dsprites-dataset/raw/master/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz"
+        try:
+            dataset_zip = np.load(
+                self.file_loc, encoding='bytes', allow_pickle=True)
+        except FileNotFoundError:
+            print("Dsprite Dataset Not Found, Downloading...")
+            request.urlretrieve(url, self.file_loc)
+            dataset_zip = np.load(
+                self.file_loc, encoding='bytes', allow_pickle=True)
+        print("Dsprites Dataset Loaded")
+        self.imgs = dataset_zip['imgs']
+        self.latents_values = dataset_zip['latents_values']
+        self.latents_classes = dataset_zip['latents_classes']
+        self.metadata = dataset_zip['metadata'][()]
+        a = 1
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, idx):
+        sample = self.imgs[idx]
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
